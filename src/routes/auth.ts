4e558d53455206f8487d31264f4a75826e354ff1
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-29 11:03:38 +0700

    feat(auth): implement login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from "express";
 import { User } from "../models/user";
 import { UserRequest, UserResponse } from "../dto/user";
-import { hashPassword } from "../utils/auth";
+import { hashPassword, verifyPassword } from "../utils/auth";
+import { createToken } from "../utils/jwt";
 import { createUser, getUser, updateUser, deleteUser } from "../services/auth";
 
 const authRouter = Router();
@@ -26,6 +27,23 @@ authRouter.post("/register", async (req: Request, res: Response) => {
 	res.json(userResponse).status(201);
 });
 
+authRouter.post("/login", async (req: Request, res: Response) => {
+	const email: string = req.body.email;
+	const password: string = req.body.password;
+	const user: User | null = await getUser(email);
+	if (!user) {
+		res.status(404).send("User not found");
+	} else {
+		const isValid: boolean = await verifyPassword(password, user.password);
+		if (!isValid) {
+			res.status(401).send("Invalid password");
+		} else {
+			const token: string = createToken(user);
+			res.json({ user, token }).status(200);
+		}
+	}
+});
+
 authRouter.get("/user-detail", async (req: Request, res: Response) => {
 	const email: string = req.body.email;
 	const user: User | null = await getUser(email);
@@ -45,37 +63,37 @@ authRouter.get("/user-detail", async (req: Request, res: Response) => {
 });
 
 authRouter.put("/update/:id", async (req: Request, res: Response) => {
-    const id: number = parseInt(req.params.id);
-    const userData: UserRequest = {
-        email: req.body.email,
-        name: req.body.name,
-        password: req.body.password,
-        role: req.body.role,
-    };
-    const updatedUser: User = await updateUser(id, userData);
-    const userResponse: UserResponse = {
-        id: updatedUser.id,
-        email: updatedUser.email,
-        name: updatedUser.name,
-        role: updatedUser.role,
-        createdAt: updatedUser.createdAt,
-        updatedAt: updatedUser.updatedAt,
-    };
-    res.json(userResponse).status(200);
+	const id: number = parseInt(req.params.id);
+	const userData: UserRequest = {
+		email: req.body.email,
+		name: req.body.name,
+		password: req.body.password,
+		role: req.body.role,
+	};
+	const updatedUser: User = await updateUser(id, userData);
+	const userResponse: UserResponse = {
+		id: updatedUser.id,
+		email: updatedUser.email,
+		name: updatedUser.name,
+		role: updatedUser.role,
+		createdAt: updatedUser.createdAt,
+		updatedAt: updatedUser.updatedAt,
+	};
+	res.json(userResponse).status(200);
 });
 
 authRouter.delete("/delete/:id", async (req: Request, res: Response) => {
-    const id: number = parseInt(req.params.id);
-    const deletedUser: User = await deleteUser(id);
-    const userResponse: UserResponse = {
-        id: deletedUser.id,
-        email: deletedUser.email,
-        name: deletedUser.name,
-        role: deletedUser.role,
-        createdAt: deletedUser.createdAt,
-        updatedAt: deletedUser.updatedAt,
-    };
-    res.json(userResponse).status(200);
+	const id: number = parseInt(req.params.id);
+	const deletedUser: User = await deleteUser(id);
+	const userResponse: UserResponse = {
+		id: deletedUser.id,
+		email: deletedUser.email,
+		name: deletedUser.name,
+		role: deletedUser.role,
+		createdAt: deletedUser.createdAt,
+		updatedAt: deletedUser.updatedAt,
+	};
+	res.json(userResponse).status(200);
 });
 
 export default authRouter;
