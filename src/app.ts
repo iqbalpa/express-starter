53b4ec345d6106a1e73583a392ef3f9c21289eac
iqbalpa <iqbalpahlevi16@gmail.com>
2024-05-07 17:42:54 +0700

    fix(register): resolve req.body issue

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,12 @@ import express, { Request, Response } from "express";
 import authRouter from "./routes/auth";
 import "dotenv/config";
 
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello World!");
